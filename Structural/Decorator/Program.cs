a579b559c1e8787e074176ad93bcd6eb12531515
Brian <brianhay3s@gmail.com>
2018-09-19 12:13:21 -0500

    Update Program.cs

--- Structural/Decorator/Program.cs
+++ Structural/Decorator/Program.cs
@@ -6,11 +6,84 @@ using System.Threading.Tasks;
 
 namespace DesignPatterns.Structural.Decorator
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+   // basic bulding block for all Components at runtime
+	interface IComponent
+	{
+		string Get();
+	}
 
-        }
-    }
+	class ConcreteComponent : IComponent
+	{
+		public string Get()
+		{
+			return "1";
+		}
+	}
+
+	// create abstract decorator all 'decorating classes' will inherit from
+	abstract class ComponentDecorator : IComponent
+	{
+		// private reference to ConcreteComponent being decorated
+		private readonly IComponent _component;
+
+		public ComponentDecorator(IComponent component)
+		{
+			_component = component;
+		}
+
+		public virtual string Get()
+		{
+			return _component.Get();
+		}
+	}
+
+	class ConcreteComponentDecoratorA : ComponentDecorator
+	{
+		public ConcreteComponentDecoratorA(IComponent component) : base(component)
+		{
+		}
+
+		public string Decorate(string result)
+		{
+			return "2" + result;
+		}
+
+		public override string Get()
+		{
+			var result = base.Get();
+			return Decorate(result);
+		}
+	}
+
+	class ConcreteComponentDecoratorB : ComponentDecorator
+	{
+		public ConcreteComponentDecoratorB(IComponent page) : base(page)
+		{
+		}
+
+		public string Decorate(string result)
+		{
+			return "3" + result;
+		}
+
+		public override string Get()
+		{
+			var result = base.Get();
+			return Decorate(result);
+		}
+	}
+
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IComponent component = new ConcreteComponent();
+			component = new ConcreteComponentDecoratorA(component);
+			component = new ConcreteComponentDecoratorB(component);
+			var result = component.Get();
+
+			Console.WriteLine(result);
+		}
+	}
 }
